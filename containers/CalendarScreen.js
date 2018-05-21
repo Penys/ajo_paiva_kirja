8b90cedff13b6102633368151e39f54b07c36dd1
anttinym <nyman.antti@gmail.com>
2018-05-21 16:49:02 +0300

    added all 3 screens to navigation

--- containers/CalendarScreen.js
+++ containers/CalendarScreen.js
@@ -1,6 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
-const CalendarScreen = () => (<Text>Calendar</Text>)
+const CalendarScreen = () => (
+  <View style={styles.container}>
+    <Text>Calendar</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 export default CalendarScreen
--- containers/NewItemScreen.js
+++ containers/NewItemScreen.js
@@ -1,6 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
-const NewItemScreen = () => (<Text>New Item</Text>)
+const NewItemScreen = () => (
+  <View style={styles.container}>
+    <Text>New Item</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 export default NewItemScreen
